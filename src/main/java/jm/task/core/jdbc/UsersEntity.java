c431d73d275357ea08b4175b873c7335a58aba0a
Amgalan <darzhaev@gmail.com>
2022-01-08 00:35:11 +0800

    Added

---
+++ src/main/java/jm/task/core/jdbc/UsersEntity.java
@@ -0,0 +1,77 @@
+package jm.task.core.jdbc;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS", schema = "springDB", catalog = "")
+public class UsersEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "ID", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "NAME", nullable = true, length = 45)
+    private String name;
+    @Basic
+    @Column(name = "LASTNAME", nullable = true, length = 45)
+    private String lastname;
+    @Basic
+    @Column(name = "AGE", nullable = true)
+    private Integer age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+        if (age != null ? !age.equals(that.age) : that.age != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        return result;
+    }
+}
